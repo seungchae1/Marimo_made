33c701c5aec102053b259078a71a7385f455c1d6
xodus05 <s2121@e-mirim.hs.kr>
2022-11-08 10:56:44 +0900

    test

--- Marimo/src/Marimo/day_marimo.java
+++ Marimo/src/Marimo/day_marimo.java
@@ -5,15 +5,15 @@ import java.awt.event.*;
 
 import javax.swing.*;
 
-public class marimo  extends JFrame/*여기있는 이미지를 프레임에 그려줄거임.*/{
-	private Image background=new ImageIcon(marimo.class.getResource("../img/KakaoTalk_20221025_111559538_01.jpg")).getImage();//배경이미지
+public class day_marimo  extends JFrame/*여기있는 이미지를 프레임에 그려줄거임.*/{
+	private Image background=new ImageIcon(day_marimo.class.getResource("../img/KakaoTalk_20221025_111559538_01.jpg")).getImage();//배경이미지
  
-	public marimo() {
+	public day_marimo() {
 		homeframe();
 	}
 	public void homeframe() {
 		setTitle("마리모 키우기");//타이틀
-		setSize(768,1024);
+		setSize(725,1024);
 		setResizable(false);//창의 크기를 변경하지 못하게
 		setLocationRelativeTo(null);//창이 가운데 나오게
 		setLayout(null);//레이아웃을 내맘대로 설정가능하게 해줌.
@@ -24,6 +24,6 @@ public class marimo  extends JFrame/*여기있는 이미지를 프레임에 그려줄거임.*/{
 		g.drawImage(background, 0, 0, null);//background를 그려줌
 	}
 	public static void main(String args[]) {
-		new marimo();
+		new day_marimo();
 	}
 }

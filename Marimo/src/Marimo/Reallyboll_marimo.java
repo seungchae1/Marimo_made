510c798ab86c62813d5ac474783f88e71a4e963b
xodus05 <s2121@e-mirim.hs.kr>
2022-11-29 07:25:00 +0900

    에러

--- Marimo/src/Marimo/Reallyboll_marimo.java
+++ Marimo/src/Marimo/Reallyboll_marimo.java
@@ -5,23 +5,24 @@ import java.awt.event.*;
 
 import javax.swing.*;
 
-public class Reallyboll_marimo extends JFrame/*여기있는 이미지를 프레임에 그려줄거임.*/ implements ActionListener{
-	private Image background=new ImageIcon(Reallyboll_marimo.class.getResource("../img/boll_marimo.png")).getImage();//배경이미지
+public class Reallyboll_marimo extends JFrame/* 여기있는 이미지를 프레임에 그려줄거임. */ implements ActionListener {
+	private Image background = new ImageIcon(Reallyboll_marimo.class.getResource("../img/boll_marimo.png")).getImage();// 배경이미지
 	private Image marimo = new ImageIcon(Reallyboll_marimo.class.getResource("../img/marimo.png")).getImage();
-	private Image rb = new ImageIcon(Reallyboll_marimo.class.getResource("../img/icon_reallyboll.png")).getImage();
+	private ImageIcon rb = new ImageIcon(Reallyboll_marimo.class.getResource("../img/icon_reallyboll.png"));
 	private ImageIcon btn_img = new ImageIcon(Reallyboll_marimo.class.getResource("../img/icon_reallyboll.png"));
 	private ImageIcon btn_img2 = new ImageIcon(Reallyboll_marimo.class.getResource("../img/icon_medicine.png"));
 	private ImageIcon btn_img3 = new ImageIcon(Reallyboll_marimo.class.getResource("../img/icon_refresh.png"));
 	private ImageIcon btn_img4 = new ImageIcon(Reallyboll_marimo.class.getResource("../img/icon_shower.png"));
 	private ImageIcon btn_sun = new ImageIcon(day_marimo.class.getResource("../img/sun.png"));
-	
+
 	JPanel main_panel; // 버튼을 붙여질 메인 패널 선언
 	JPanel sun_panel;
-	JButton btn, btn2, btn3, btn4, sun;
-	
+	JButton btn, btn2, btn3, btn4, sun, real_b;
+
 	public Reallyboll_marimo() {
 		homeframe();
 	}
+
 	public void homeframe() {
 		setTitle("마리모 키우기");// 타이틀
 		setResizable(false);// 창의 크기를 변경하지 못하게
@@ -59,7 +60,7 @@ public class Reallyboll_marimo extends JFrame/*여기있는 이미지를 프레임에 그려줄
 		btn2.setBackground(new Color(255, 255, 213)); // 약
 		btn3.setBackground(new Color(255, 255, 213)); // 냉장고
 		btn4.setBackground(new Color(255, 255, 213)); // 샤워
-		sun.setBackground(new Color(255,255,213)); // 달
+		sun.setBackground(new Color(255, 255, 213)); // 달
 
 		btn4.addActionListener(this);
 		sun.addActionListener(this);
@@ -74,23 +75,36 @@ public class Reallyboll_marimo extends JFrame/*여기있는 이미지를 프레임에 그려줄
 		add(sun_panel); // 메인 프레임에 썬패널 붙여주는 작업
 		setVisible(true); // 프레임 보이게 하기
 	}
-	public void paint(Graphics g) {//그리는 함수
-		g.drawImage(background, 0, 0, null);//background를 그려줌
-		g.drawImage(marimo, 250, 500, null);
-		g.drawImage(rb, 500, 200, null);
-	}
-	public static void main(String args[]) {
-		new Reallyboll_marimo();
-	}
-	
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == btn4) {
-			Shower_marimo a = new Shower_marimo();
-			setVisible(false);
+
+	class MyPanel extends JPanel {
+		@Override
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.drawImage(background, 0, 0, null);// background를 그려줌
+			g.drawImage(marimo, 250, 500, null);
 		}
-		if (e.getSource() == sun) {
-			day_marimo b = new day_marimo();
-			setVisible(false);
+
+		public void paint(Graphics g) {// 그리는 함수
+			g.drawImage(background, 0, 0, null);// background를 그려줌
+			g.drawImage(marimo, 250, 500, null);
+			g.drawImage(rb, 500, 200, null);
+		}
+
+		public static void main(String args[]) {
+			new Reallyboll_marimo();
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			if (e.getSource() == btn4) {
+				Shower_marimo a = new Shower_marimo();
+				setVisible(false);
+			}
+			if (e.getSource() == sun) {
+				day_marimo b = new day_marimo();
+				setVisible(false);
+			}
 		}
 	}
 }
